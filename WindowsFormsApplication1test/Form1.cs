d3e25e2799f9f3e315f8e79693c8a132de998840
alonso <alonso@wavegis.com.tw>
2014-10-16 16:17:35 +0800

    initial

---
+++ WindowsFormsApplication1test/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Gma.UserActivityMonitor;
+
+namespace WindowsFormsApplication1test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            this.FormClosing +=Form1_FormClosing;
+            HookManager.KeyPress += HookManager_KeyPress;
+            
+        }
+
+        void HookManager_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case '+':
+                    this.Opacity -= 1/100.0;
+                    break;
+                case '-':
+                    this.Opacity += 1/100.0;
+                    break;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        void Form1_Load(object sender, EventArgs e)
+        {
+            WindowWrapper.SetWindowLong(this.Handle, WindowWrapper.GWL.ExStyle, WindowWrapper.WS_EX.Layered | WindowWrapper.WS_EX.Transparent);
+        }
+    }
+}
---
+++ WindowsFormsApplication1test/WindowWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace WindowsFormsApplication1test
+{
+    public class WindowWrapper
+    {
+        [DllImport("user32")]
+        public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
+        [DllImport("user32.dll")]
+        public static extern bool SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte alpha, LWA dwFlags);
+        [DllImport("user32")]
+        public static extern int SetWindowLong(IntPtr hWnd, GWL nIndex, WS_EX dsNewLong);
+
+        public enum GWL
+        {
+            ExStyle = -20
+        }
+
+        public enum LWA
+        {
+            Alpha = 2,
+            ColorKey = 1
+        }
+
+        public enum WS_EX
+        {
+            Layered = 0x80000,
+            Transparent = 0x20
+        }
+    }
+}
