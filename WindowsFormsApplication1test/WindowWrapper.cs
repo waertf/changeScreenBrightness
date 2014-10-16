984c848fbcc61aa971cc664bdb99ba4b3367aea0
alonso <alonso@wavegis.com.tw>
2014-10-16 16:52:17 +0800

    change from GetWindowLong to GetWindowLongPtr for x64 compatible

--- WindowsFormsApplication1test/WindowWrapper.cs
+++ WindowsFormsApplication1test/WindowWrapper.cs
@@ -9,7 +9,7 @@ namespace WindowsFormsApplication1test
     public class WindowWrapper
     {
         [DllImport("user32")]
-        public static extern int GetWindowLong(IntPtr hWnd, GWL nIndex);
+        public static extern int GetWindowLongPtr(IntPtr hWnd, GWL nIndex);//GetWindowLong
         [DllImport("user32.dll")]
         public static extern bool SetLayeredWindowAttributes(IntPtr hWnd, int crKey, byte alpha, LWA dwFlags);
         [DllImport("user32")]
